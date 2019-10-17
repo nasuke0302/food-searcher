f572f6e2ab11bd73f291af3fea8fa535ad72aa99
Alberto Esquivel <albertoesquivelanias@gmail.com>
2019-10-17 17:02:00 -0400

    added navigator

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default () => {
+    return (
+        <View>
+            <Text> textInComponent </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+});
